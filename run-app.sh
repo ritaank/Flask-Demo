6338db95143368a5c213f533d13662ca73d26c82
ritaank <ritaank@gmail.com>
2020-05-27 06:08:27 +0000

    updated permissions

--- run-app.sh
+++ run-app.sh
@@ -9,7 +9,7 @@ sudo apt -y install python3-flask
 sudo apt-get -y install python3-sqlalchemy
 pip3 install Flask-SQLAlchemy
 pip3 install -r requirements.txt
-python3 -m pip install -R requirements.txt
+sudo apt -y install curl
 
 export FLASK_APP="app.py"
 flask run -h localhost -p $1
