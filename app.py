ee052657555a8cf7f2adae84b9f1b406377017b2
Ritaank Tiwari <ritaank@mit.edu>
2020-05-26 17:36:43 -0700

    cleaned up, added comments

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+## Configuration
+
 app = Flask(__name__)
-database_uri_mysql = "mysql://sql9343372:M9n5hMmtQP@sql9.freemysqlhosting.net:3306/sql9343372"
+# database_uri_mysql = "mysql://sql9343372:M9n5hMmtQP@sql9.freemysqlhosting.net:3306/sql9343372"
 database_uri_postgres = "postgres://kqpakbfg:HKfT25s4G3yf89SxJQXkH-pxVmIwsWnS@ruby.db.elephantsql.com:5432/kqpakbfg"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri_postgres
@@ -11,13 +12,15 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 db = SQLAlchemy(app)
 
+## Tables
 
 class Doctor(db.Model):
-    # __tablename__ = 'doctor'
+    __tablename__ = 'doctor'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     reviews = db.relationship('Review', backref='doctor', lazy=True)
 
+    #returns dictionary output of doctor and all reviews of that doctor
     def as_dict(self):
         ans = {c.name: getattr(self, c.name) for c in self.__table__.columns}
         ans["reviews"] = [review.as_dict() for review in self.reviews]
@@ -25,20 +28,24 @@ class Doctor(db.Model):
 
 
 class Review(db.Model):
-    # __tablename__ = 'review'
+    __tablename__ = 'review'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(120), nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
 
+    #returns dictionary of review object
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+
+##Routes
 
 # Create a doctor
 @app.route('/doctors', methods=['POST'])
 def add_doctor():
 
-    data = request.get_json()  # data: dict
+    #data: dict where data['name'] = doctor name
+    data = request.get_json()
     doctor = Doctor(name=data['name'])
     db.session.add(doctor)
     db.session.commit()
@@ -49,12 +56,14 @@ def add_doctor():
 @app.route('/doctors/<doctor_id>/reviews', methods=['POST'])
 def add_review(doctor_id):
 
+    #data: dict where data['description'] = text of review
     data = request.get_json()
 
     review = Review(description=data['description'], doctor_id=doctor_id)
     db.session.add(review)
     db.session.commit()
 
+    #map the review under the existing doctor
     doctor = Doctor.query.filter_by(id=doctor_id).first()
     doctor.reviews.append(review)
 
@@ -81,7 +90,7 @@ def delete_review(doctor_id, review_id):
 
     doctor = Doctor.query.filter_by(id=doctor_id).first()
     review = Review.query.filter_by(id=review_id).first()
-    db.session.delete(review)
+    db.session.delete(review) #removes the review from the doctor as well
     db.session.commit()
 
     return "Deleted review \"" + str(review.description) + "\" " \
@@ -92,8 +101,12 @@ def delete_review(doctor_id, review_id):
 def delete_doctor(doctor_id):
 
     doctor = Doctor.query.filter_by(id=doctor_id).first()
+
+    #delete all of the doctor's reviews
     for review in doctor.reviews:
         db.session.delete(review)
+    
+    #then remove the doctor
     db.session.delete(doctor)
     db.session.commit()
 
